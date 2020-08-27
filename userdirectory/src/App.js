b11ef9f407c09144939bb6ca343a9544825eddbc
Ayodeji Amosun <ayodeji.amosun04@gmail.com>
2020-08-27 11:34:31 -0500

    finished app.js and created components folder!

--- userdirectory/src/App.js
+++ userdirectory/src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FriendCard from "./components/EmployeeCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import employees from "./Employee.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    employees,
+    search:"",
+    name:"term"
+  };
+
+  removeEmployee = id => {
+    const employees = this.state.employees.filter(employee => employee.id !== id);
+    this.setState({ employees });
+  };
+
+  searchByName = name => {
+    const search = this.state.employees.filter(employee => employee.name !== name);
+    this.setState({ search });
+    console.log(search);
+  }
+  handleInputChange = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value
+    });
+  };
+  handleSortingByname= () => {
+
+  }
+
+  render() {
+    return (
+      <Wrapper
+      >
+        <Title>Friends List</Title>
+        {this.state.employees.map(employee => (
+          <FriendCard
+            removeEmployee={this.removeEmployee}
+            id={employee.id}
+            key={employee.id}
+            name={employee.name}
+            image={employee.image}
+            role={employee.role}
+            email={employee.email}
+            phone={employee.phone}
+          />
+        ))}
+          <button className="inline-block" onClick={this.handleSortingByname}>SortingByName</button>
+      </Wrapper>
+    );
+  }
 }
+
+export default App;
 
 export default App;
